cf7e541e42a8022fa2d5fcf58212e8484ea5eeb6
Akhil.T <get4gnetworkA>
2023-11-16 12:33:21 +0530

    some

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-import { CountUp } from 'https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js'
+var a = 0;
+$(window).scroll(function() {
 
-function countStart(){
-  const $counters = document.querySelectorAll(".js-count-up"),
-        options = {
-          useEasing: true,
-          useGrouping: true,
-          separator: ",",
-          decimal: "."
-        };
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
 
-  $counters.forEach( (item) => {
-    const value = item.dataset.value;
-    const counter = new CountUp(item, value, options);
-    counter.start();
-  });
-}
+        {
 
-new Waypoint({
-  element: document.querySelector('.level'),
-  handler: function() {
-    countStart()
-    //this.destroy() //for once
-  },
-  offset: '50%'
+          duration: 7000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
 });
